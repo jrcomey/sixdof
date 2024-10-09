0fcc6ed20d76e02176f9d74c82b161f2a430c04a
Jack <jrcomey@github.com>
2024-10-08 17:50:51 -0700

    Added json loading

--- src/graphical.rs
+++ src/graphical.rs
@@ -25,7 +25,6 @@ impl Default for GraphicalData {
 }
 
 impl GraphicalData {
-
     pub fn new(name: &str, filepath: &str, color: [f64; 4]) -> Self {
         GraphicalData {
             name: name.to_string(),
@@ -33,6 +32,55 @@ impl GraphicalData {
             color: color,
             ..Default::default()
         }
+    }
+
+    pub fn from_json(json_file: &Value) -> Self {
+        let name = json_file["Name"].as_str().unwrap();
+        let object_file_path = json_file["ObjectFilePath"].as_str().unwrap();
+        // let scale: [f64; 3]=json_file["Scale"].as_array().unwrap().into_iter().map(|x| x.as_f64().unwrap()).collect();
+
+        let pos_temp: Vec<_> = json_file["Position"]
+            .as_array()
+            .unwrap()
+            .into_iter()
+            .map(|x| x.as_f64().unwrap())
+            .collect();
+        let mut pos_vec = [pos_temp[0], pos_temp[1], pos_temp[2]];
+
+        let or_temp: Vec<_> = json_file["Orientation"]
+            .as_array()
+            .unwrap()
+            .into_iter()
+            .map(|x| x.as_f64().unwrap())
+            .collect();
+        let mut or_vec = [or_temp[0], or_temp[1], or_temp[2]];
+
+        let rot_temp: Vec<_> = json_file["Rotation"]
+            .as_array()
+            .unwrap()
+            .into_iter()
+            .map(|x| x.as_f64().unwrap())
+            .collect();
+        let mut rot_vec = [rot_temp[0], rot_temp[1], rot_temp[2]];
+
+        let scale_temp: Vec<_> = json_file["Scale"]
+            .as_array()
+            .unwrap()
+            .into_iter()
+            .map(|x| x.as_f64().unwrap())
+            .collect();
+        let mut scale_vec = [scale_temp[0], scale_temp[1], scale_temp[2]];
+
+        let color_temp: Vec<_> = json_file["Color"]
+            .as_array()
+            .unwrap()
+            .into_iter()
+            .map(|x| x.as_f64().unwrap())
+            .collect();
+        let mut color_vec = [color_temp[0], color_temp[1], color_temp[2], color_temp[3]];
+
+        GraphicalData { name: name.to_string(), filepath: object_file_path.to_string(), relative_position: pos_vec, orientation: or_vec, rotation: rot_vec, color: color_vec, scale: scale_vec }
+
     }
 
     pub fn get_model_value(&self) -> Value {
