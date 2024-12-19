24672b0053dbe1a42a3191050a5bc77b543384dc
Jack <jrcomey@github.com>
2024-12-18 23:53:48 -0800

    Tweaks to unit tests, added linspace

--- src/main.rs
+++ src/main.rs
@@ -47,6 +47,7 @@ mod environments;
 mod fc;
 mod graphical;
 mod components;
+use crate::components::ComponentPart;
 fn main() -> Result<(), std::io::Error> {
     let t_start = std::time::Instant::now();
     std::env::set_var("RUST_LOG", "trace");                                 // Initialize logger
@@ -70,9 +71,11 @@ fn main() -> Result<(), std::io::Error> {
     sim.run();
     // // println!("{}", sim.scene_initialization_to_datacom());
     info!("Program End!");
+    // let name = sim.get_object(0).get_name();
     info!("Final Position: {}", sim.get_object(0).get_position());
     println!("Program finished in {:.2?} in {:} steps", t_start.elapsed(), sim.steps);
-    sim.record_run("data/runs");
+    // let filepath = format!("/data/runs/{}", sim.)
+    sim.record_run("data/runs/test_drone");
     info!("Run recorded. Saved to data/runs");
 
     Ok(())
@@ -80,6 +83,10 @@ fn main() -> Result<(), std::io::Error> {
 
 #[cfg(test)]
 mod tests {
+    use core::time;
+
+    use components::{ComponentPart, IdealThruster};
+
     use super::*;
 
     #[test]
@@ -139,60 +146,60 @@ mod tests {
     }
 
     #[test]
-    fn RK4_gravity_test_small_dt() {
-        let mut sim = sixdof::Sim::new(0.001);
-        let mut drone = Box::new(sixdof::Vehicle::<4>::new());
-        let A_new = na::SMatrix::<f64, 12, 12>::from_row_slice(&[
-            0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0,
-            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0,
-            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0,
-            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-        ]);
-        let state_new = na::SMatrix::<f64, 12, 1>::from_row_slice(&[
-            0.0,    // x
-            0.0,    // y
-            0.0,    // z
-            0.0,    // xdot
-            0.0,    // ydot
-            0.0,    // zdot
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            0.0
-        ]);
-        let C_new = na::SMatrix::<f64, 6, 12>::from_row_slice(&[
-            1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0,
-        ]);
-        drone.set_A(
-            A_new
-        );
+    // fn RK4_gravity_test_small_dt() {
+    //     let mut sim = sixdof::Sim::new(0.001);
+    //     let mut drone = Box::new(sixdof::Vehicle::<4>::new());
+    //     let A_new = na::SMatrix::<f64, 12, 12>::from_row_slice(&[
+    //         0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+    //         0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+    //         0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+    //         0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+    //         0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+    //         0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+    //         0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0,
+    //         0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0,
+    //         0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0,
+    //         0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+    //         0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+    //         0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+    //     ]);
+    //     let state_new = na::SMatrix::<f64, 12, 1>::from_row_slice(&[
+    //         0.0,    // x
+    //         0.0,    // y
+    //         0.0,    // z
+    //         0.0,    // xdot
+    //         0.0,    // ydot
+    //         0.0,    // zdot
+    //         0.0,
+    //         0.0,
+    //         0.0,
+    //         0.0,
+    //         0.0,
+    //         0.0
+    //     ]);
+    //     let C_new = na::SMatrix::<f64, 6, 12>::from_row_slice(&[
+    //         1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+    //         0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+    //         0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+    //         0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+    //         0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0,
+    //         0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0,
+    //     ]);
+    //     drone.set_A(
+    //         A_new
+    //     );
 
-        // let Rocket = Box::new(sixdof::Rocket::new());
-        drone.set_state(state_new);
-        sim.add_object(drone);
-        let t = 10.0;
-        sim.run_until(t);
-        let final_position = sim.get_object(0).get_position()[2];
-        let final_predicted = (1.0/2.0) * t*t * (-9.81);
+    //     // let Rocket = Box::new(sixdof::Rocket::new());
+    //     drone.set_state(state_new);
+    //     sim.add_object(drone);
+    //     let t = 10.0;
+    //     sim.run_until(t);
+    //     let final_position = sim.get_object(0).get_position()[2];
+    //     let final_predicted = (1.0/2.0) * t*t * (-9.81);
 
-        assert!(0.9999*final_predicted > final_position);
-        assert!(1.0001*final_predicted < final_position);
-    }
+    //     assert!(0.9999*final_predicted > final_position);
+    //     assert!(1.0001*final_predicted < final_position);
+    // }
 
     #[test]
     fn uncontrolled_position_input() {
@@ -206,5 +213,27 @@ mod tests {
         assert!(final_position > 0.5*final_unassisted);
     }
 
+    #[test]
+    fn simple_thruster_step_response() {
+        let mut thruster = IdealThruster::new();
+        let time_vec = linspace(0.0, 10.0, 100);
+        let result_vec: Vec<f64> = vec![];
+        thruster.set_time_const(0.01);
 
+        thruster.set_force(1.0);
+        for t in time_vec {
+            thruster.update_to_time(vec![0.0], t);
+            debug!("Time: {}", t);
+            debug!("Force: {}", thruster.get_force_on_parent()[3]);
+        }
+    }
+
+
+}
+
+fn linspace(start: f64, end: f64, num_points: usize) -> Vec<f64> {
+    let step = (end - start) / (num_points as f64 - 1.0);
+    (0..num_points)
+        .map(|i| start + (i as f64) * step)
+        .collect()
 }
