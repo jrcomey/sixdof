103471dee323f3b4c94cf32d2c755a4ea2482b81
Jack <jrcomey@github.com>
2024-12-18 23:56:22 -0800

    tweaked scaling factor

--- src/datatypes.rs
+++ src/datatypes.rs
@@ -1,4 +1,4 @@
 pub type State = na::SMatrix<f64, 12, 1>;
 pub type Inputs<const U: usize> = na::SMatrix<f64, U, 1>; 
 
-pub const GRAPHICAL_SCALING_FACTOR: f64 = 1E-6;
+pub const GRAPHICAL_SCALING_FACTOR: f64 = 1E0;
