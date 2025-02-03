a5ff3919e30b30ff8319fc7274dc996ba4004096
Jack <jrcomey@github.com>
2025-02-02 18:25:33 -0800

    Added code to load neural net as flight controller

--- src/fc.rs
+++ src/fc.rs
@@ -1,4 +1,7 @@
+use std::vec;
+
 use sj::Value;
+use tract_onnx::prelude::*;
 
 use crate::datatypes::*;
 
@@ -12,6 +15,7 @@ pub struct FlightComputer<const U: usize> {
 
 pub trait FlightControl<const U: usize> {
     fn estimate_state(&mut self, actual_state: State) -> State;
+    fn guidance(&mut self, t: &f64, estimated_state: State) -> State;
     fn calculate_u(&self, current_state: State) -> Inputs<U>;
 }
 
@@ -69,6 +73,10 @@ impl<const U: usize> FlightControl<U> for FlightComputer<U> {
         // debug!("FC OUTPUT: {}", -self.K*err);
         -self.K*err
     }
+
+    fn guidance(&mut self, t: &f64, estimated_state: State) -> State {
+        State::zeros()
+    }
 }
 
 pub struct NullComputer<const U: usize> {
@@ -89,6 +97,10 @@ impl<const U: usize> FlightControl<U> for NullComputer<U> {
         return State::zeros();
     }
 
+    fn guidance(&mut self, t: &f64, estimated_state: State) -> State {
+        State::zeros()
+    }
+
     fn calculate_u(&self, current_state: State) -> Inputs<U> {
         return Inputs::<U>::zeros();
     }
@@ -99,6 +111,58 @@ pub struct NNComputer<const U: usize> {
     t_last_updated: f64,
     sensors: Vec<Sensor>,
     cmd_inputs: Inputs<U>,
+    network: SimplePlan<TypedFact, Box<dyn TypedOp>, Graph<TypedFact, Box<dyn TypedOp>>>,
+}
+
+impl<const U: usize> NNComputer<U> {
+
+    fn new_from_file(json_parsed: &Value) -> Result<Self, std::io::Error> {
+        let model = tract_onnx::onnx()
+        .model_for_path("data/todo/default_name/objects/blizzard/blizzard_controller.onnx").unwrap()
+        .into_optimized().unwrap()
+        .into_runnable().unwrap();
+
+        let nn_computer = NNComputer {
+            sample_time: 0.0,
+            t_last_updated: 0.0,
+            sensors: vec![],
+            cmd_inputs: Inputs::zeros(),
+            network: model,
+        };
+        Ok(nn_computer)
+    }
+
+
+}
+
+impl<const U: usize> FlightControl<U> for NNComputer<U>{
+    
+    fn estimate_state(&mut self, actual_state: State) -> State {
+        actual_state   
+    }
+
+    fn guidance(&mut self, t: &f64, estimated_state: State) -> State {
+        State::zeros()
+    }
+
+    fn calculate_u(&self, current_state: State) -> Inputs<U> {
+        let err: State = rotation_frame(&current_state[6], &current_state[7], &current_state[8]).transpose()
+        * (State::zeros() - current_state);
+
+        let input = tract_ndarray::Array2::from_shape_fn(
+            (1,12),
+            |(i,j)| err[(i, j)]
+        ).into_tensor();
+
+        let result = self.network.run(tvec!(input.into())).unwrap();
+
+        let output_view = result[0].to_array_view::<f64>().unwrap();
+        let output = Inputs::from_row_slice(
+            output_view.as_slice().unwrap()
+        );
+
+        return output
+    }
     
 }
 
@@ -113,4 +177,9 @@ impl Sensor {
     pub fn sense(&self, vehicle_state: State) -> State {
         return vehicle_state;
     }
+}
+
+pub trait NavComputer {
+    fn estimate_state(&mut self, true_state: &State, sensors: Vec<Sensor>) -> State;
+    // fn 
 }
